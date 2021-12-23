a35028b97dad9e751df6618955bf705e9397d607
Rakesh Baruah <rbaruah@gmail.com>
2021-12-23 08:22:39 -0600

    add about home and layout

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm proud the proud creator of this site, which I build with
+        Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
